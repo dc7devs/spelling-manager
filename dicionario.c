fdb87562c47f382fd9b72d01d355f757fd792373
Diego Silva <diegocaetano444@outlook.com>
2022-06-21 20:50:09 -0300

    [📱] Reorganize struct code

--- dicionario.c
+++ dicionario.c
@@ -1,24 +1,8 @@
 #include "dicionario.h"
 
-#define SIZE_PALAVRAS 100 // alocação dinamica
-#define LINHAS 10	  	  // alocação dinamica
-
-char * Dictionary(char keyword[]) {
+void DictionaryMain() {
 	FILE *fileDictionary;
-	
-	char dictionaryWord[SIZE_PALAVRAS];
-	char *wordVector[LINHAS]; // malloc
-	long int count;
-
-	openAndVerifyFile(&fileDictionary, "resource-file/dictionary.txt", "r");
-
-	// EXEMPLO
-	for(int i=0; i<LINHAS; i++) {
-		fgets(dictionaryWord, sizeof(dictionaryWord), fileDictionary);
-		wordVector[i] = strdup(dictionaryWord);
-
-		printf("%ld. %s", i, wordVector[i]);
-	}
+	openAndVerifyFile(&fileDictionary, "resource-file/Brazilian.txt", "r");
 
 	/* O dicionário deve ser totalmente carregado em um vetor de palavras na memória
 	 * RAM antes de ser usado, usando alocação dinâmica (os mais ousados podem tentar
@@ -26,7 +10,10 @@ char * Dictionary(char keyword[]) {
 	 */
 	// A localização das palavras no dicionário deve usar um algoritmo de busca binária.
 
-	// Procurar por keword ou a mais proxima de keyword
 	fclose(fileDictionary);
-	return *wordVector;
+}
+
+void SearchWord(char keyword[]) {
+	
+	// Procurar por keword ou a mais proxima de keyword
 }
